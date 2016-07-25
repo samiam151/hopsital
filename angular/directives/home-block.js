40d4a3c5337fbd0ba8fb18f23694be1e451d8099
samiam151 <samiam151@gmail.com>
2016-07-25 03:17:35 -0400

    Added images and "final" styling to home page

--- angular/directives/home-block.js
+++ angular/directives/home-block.js
@@ -1,7 +1,20 @@
 app.directive('homeBlock', function(){
     return {
         restrict: 'E',
-        transclude: true,
-        templateUrl: 'templates/home-blocks.html'
+        scope: {},
+        transclude: {
+            'blockimage': 'img'
+        },
+        // templateUrl: 'templates/home-blocks.html'
+        template: `
+            <div class="home-block-div">
+                <div class="home-block-img">
+                    <span ng-transclude='blockimage'></span>
+                </div>
+                <div class="home-block-content">
+                    
+                </div>
+            </div>
+        `
     }
 });
--- angular/services/DirectionsService.js
+++ angular/services/DirectionsService.js
@@ -2,37 +2,50 @@ app.service('DirectionsService', function(){
     this.topDirections = [
         {
             item: 'Learn about my medicines',
-            url: '#/medications'
+            url: '#/medications',
+            bg_img: 'static/img/medications.jpeg'
         },
          {
             item: 'Watch videos picked just for me',
-            url: '#/videos'
+            url: '#/videos',
+            bg_img: 'static/img/skeleton_muscle.jpg'
         }, {
             item: 'Rate my pain',
-            url: '#/pain'
+            url: '#/pain',
+            bg_img: 'static/img/cactus.jpeg'
         }
     ];
 
     this.bottomDirections = [
         {
-            item: 'See my medicines'
+            item: 'See my medicines',
+            color: '#E74C3C',
+            glyph: 'leaf'
         },
         {
-            item: 'Browse health videos'
+            item: 'Browse health videos',
+            color: '#5499C7',
+            glyph: 'facetime-video'
         },
         {
-            item: 'Watch TV'
+            item: 'Watch TV',
+            color: '#F5B041',
+            glyph: 'picture'
         },
         {
-            item: 'Listen to Music'
+            item: 'Listen to Music',
+            color: '#45B39D',
+            glyph: 'music'
         },
         {
-            item: 'Order a meal'
+            item: 'Order a meal',
+            color: '#34495E',
+            glyph: 'cutlery'
         },
         {
-            item: 'Share a compliment'
+            item: 'Share a compliment',
+            color: '#AAB7B8',
+            glyph: 'comment'
         }
     ];
-
-    // this.allDirections = this.topDirections.concat(this.bottomeDirections);
 });
